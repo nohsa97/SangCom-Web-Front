4f46d818a4767d1b8f175e1387e487f66c2fd069
nohsa97 <81568105+nohsa97@users.noreply.github.com>
2022-03-30 03:09:46 +0900

    경은이+합본

--- SangCom_web/js/login.js
+++ SangCom_web/js/login.js
@@ -2,19 +2,23 @@ let id = document.getElementById('member_ID');
  let pw = document.getElementById('member_password');
 
 
-  document.getElementById('login_btn').onclick = function() {
-  {
+   function loginBtn() {
      if(id.value=="")
      {
        alert('아이디를 입력 하십시오');
      }
+
      else if(pw.value=="")
      {
        alert('비밀번호를 입력 하십시오');
      }
 
-    else {
-      alert("어서오십시오. SangComTest님.")
+     else if(id.value!='sangcomtest' || pw.value!='test') {
+      alert('아이디 비밀번호가 틀렸습니다.')
+     }
+
+    if(id.value=='sangcomtest' && pw.value=='test') {
+      alert("어서오십시오. SangComTest님.");
+      location.href = "main.html";
     }
-  }
 }
